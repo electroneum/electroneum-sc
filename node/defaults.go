f776e66c21d1c79b4e22f32cc1dc378307193a2f
andrepatta <andre.patta@electroneum.com>
2022-06-14 18:34:45 -0300

    node: change default data dir

--- node/defaults.go
+++ node/defaults.go
@@ -74,19 +74,19 @@ func DefaultDataDir() string {
 	if home != "" {
 		switch runtime.GOOS {
 		case "darwin":
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "Electroneum")
 		case "windows":
 			// We used to put everything in %HOME%\AppData\Roaming, but this caused
 			// problems with non-typical setups. If this fallback location exists and
 			// is non-empty, use it, otherwise DTRT and check %LOCALAPPDATA%.
-			fallback := filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			fallback := filepath.Join(home, "AppData", "Roaming", "Electroneum")
 			appdata := windowsAppData()
 			if appdata == "" || isNonEmptyDir(fallback) {
 				return fallback
 			}
-			return filepath.Join(appdata, "Ethereum")
+			return filepath.Join(appdata, "Electroneum")
 		default:
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".electroneum")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
